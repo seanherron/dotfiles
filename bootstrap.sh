5d5f256a498adb6f82f9709a1462b77f3458f5bd
Sean Herron <sean@herron.io>
2014-09-02 00:00:02 -0400

    inited bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,1 +1,27 @@
-git pull origin master;
+# git pull origin master;
+echo "hello";
+
+function zpreztoInit {
+	cp .zprezto/runcoms/z* .
+	for f in z*
+	do
+		mv $f .$f
+	done
+}
+
+function doIt() {
+	zpreztoInit;
+	unset zpreztoInit;
+	rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" \
+		--exclude "README.md" -avh --no-perms . ~;
+	zsh
+	chsh -s /bin/zsh
+}
+
+read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
+echo "";
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+	doIt;
+fi;
+
+unset doIt;
